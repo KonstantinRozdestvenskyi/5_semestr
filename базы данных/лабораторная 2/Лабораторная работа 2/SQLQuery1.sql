dd820a2399417c5d0a0a8e22ec26d13773374c63
KonstantinRozdestvenskyi <114247441+KonstantinRozdestvenskyi@users.noreply.github.com>
2023-10-07 18:48:49 +0300

    Delete базы данных/лабораторная 2/Лабораторная работа 2 directory

--- базы данных/лабораторная 2/Лабораторная работа 2/SQLQuery1.sql
+++
@@ -1,8 +0,0 @@
-USE AdventureWorks
-SELECT SalesPersonID, Bonus FROM Sales.SalesPerson
-	ORDER BY Bonus DESC
-SELECT TOP 4 SalesPersonID, Bonus FROM Sales.SalesPerson
-	ORDER BY Bonus DESC
-SELECT TOP 4 WITH TIES SalesPersonID, Bonus 
-	FROM Sales.SalesPerson
-	ORDER BY Bonus DESC
